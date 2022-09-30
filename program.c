33577dd9778c26cf73fa4d7d281b91524c28dfea
Nasir Khan <nasirkhan.cse20@gmail.com>
2022-09-30 23:04:43 +0600

    added delete all features

--- program.c
+++ program.c
@@ -8,18 +8,23 @@
 // some global variables
 int i,j;
 int LineLenth;
+char StudentID[10];
 FILE *AllStudents;
 FILE *AllCourses;
+FILE *ExistingAllStudents;
+FILE *TempAllStudents;
+FILE *ExistingAllCourses;
+FILE *TempAllCourses;
 // end
 
 bool IsRunning = true;
 void Menu();
 void AddNewStudent();
 void ShowAllStudents();
-int SearchStudent(char StudentId[10]);
+int  SearchStudent(char StudentId[10]);
 void EditStudent(int StudentId);
-void DeleteStudent(int StudentId);
-void DeleteAllStudents();
+int  DeleteStudent(char StudentID);
+int DeleteAllStudents();
 bool IsAlreadyExists(char *GivenLine[30],char InfoType);
 void ErrorAndRestart(char *Error[100]);
 void UserGuideline();
@@ -43,22 +48,41 @@ int main(){
             case 3:{
                     system("cls");
                     printf("\n\t\t **** Search Students ****\n\n");
-
-                    char StudentID[10];
+                    printf("Enter The Student ID: ");
                     scanf("%s",StudentID);
                     int IsFound = SearchStudent(StudentID);
-                    printf("%d",IsFound);
+                    if(!IsFound){
+                        printf("No Student Found\n");
+                    }
                     break;
                }
             case 4:
                 //EditStudent();
                 break;
             case 5:
-                //DeleteStudent();
+                scanf("%s",StudentID);
+                int isDeleted = DeleteStudent(StudentID);
                 break;
             case 6:
-                //DeleteAllStudents();
-                break;
+                {
+                    char Sure = 'N';
+                    getchar();
+                    printf("Are you sure want to delete all the students? (Y/N): ");
+                    scanf("%c",&Sure);
+                    if(Sure == 'Y' || Sure == 'y'){
+                        int IsDeleted = DeleteAllStudents();
+                        if(IsDeleted){
+                            printf("Success\n");
+                        }else{
+                            printf("Your Data is Safe.\n");
+                        }
+                    }else{
+                        printf("Your Data is Safe.\n");
+                    }
+                    break;
+                }
+
+
             case 7:
                 //ClearWindow();
                 break;
@@ -75,7 +99,6 @@ int main(){
 
     return 0;
 } // end main function
-
 
 void Menu(){
     printf("\n\n\t***welcome to the student management system***\n\n");
@@ -94,7 +117,6 @@ void Menu(){
     printf("\t\t=======================\n");
     printf("\t\tEnter The Choice: ");
 } // end menu
-
 
 void AddNewStudent(){
     system("cls");
@@ -140,6 +162,10 @@ void AddNewStudent(){
     strcat(NewStudent,NumberOfCoursesStr);
     strcat(NewStudent,"\n");
 
+    AllCourses  = fopen("data/All-Courses.txt","a");
+    StudentCourses[0] = 0;
+    strcat(StudentCourses,IDStr);
+
     for(i=0;i<NumberOfCourses;i++){
         printf("Enter Course %d The Code: ",i+1);
         scanf(" %[^\n]s",&CourseCode);
@@ -147,22 +173,25 @@ void AddNewStudent(){
         printf("Enter Course %d The Name: ",i+1);
         scanf(" %[^\n]s",&CourseName);
 
-        StudentCourses[0] = 0;
-        strcat(StudentCourses,IDStr);
+
         strcat(StudentCourses,"|");
         strcat(StudentCourses,CourseCode);
         strcat(StudentCourses,"|");
         strcat(StudentCourses,CourseName);
         strcat(StudentCourses,"\n");
+
+
     }
 
     AllStudents = fopen("data/All-Students.txt","a");
     fprintf(AllStudents,NewStudent);
     fclose(AllStudents);
 
-    AllCourses  = fopen("data/All-Courses.txt","a");
     fprintf(AllCourses,StudentCourses);
+    strcat(StudentCourses,"\n");
     fclose(AllCourses);
+
+
 
     printf("Student Added Success\n");
 }
@@ -216,6 +245,7 @@ void ShowAllStudents(){
 int SearchStudent(char StudentID[10]){
     char ThisStudetID[10];
     int StudentFound = 1;
+    int Pipe;
     AllStudents = fopen("data/All-Students.txt","r");
     LineLenth = 200;
     char Student[LineLenth];
@@ -235,7 +265,7 @@ int SearchStudent(char StudentID[10]){
 
     if(StudentFound){
         printf("ID: ");
-        int Pipe = 0;
+        Pipe = 0;
         for(i=0;i<strlen(Student);i++){
             if(Student[i] == '|'){
                 Pipe++;
@@ -255,16 +285,110 @@ int SearchStudent(char StudentID[10]){
         }
     }
 
+    int CourseFound = 1;
+    AllCourses = fopen("data/All-Courses.txt","r");
+    char Course[LineLenth];
+
+    while(fgets(Course,LineLenth,AllCourses)){
+        for(i=0;Course[i] != '|'; i++){
+            ThisStudetID[i] = Course[i];
+        }
+        ThisStudetID[i] = '\0';
+        if(strcmp(StudentID,ThisStudetID) == 0){
+            CourseFound = 1;
+            break;
+        }else{
+            CourseFound = 0;
+        }
+    }
+
+    if(CourseFound){
+        Pipe = 1;
+        for(i=0; i<strlen(Course); i++){
+            if(Course[i] == '|'){
+                Pipe++;
+                if(Pipe > 2 && Pipe %2 != 0){
+                    printf("\nCourse Name: ");
+                }else{
+                    printf("\nCourse Code: ");
+                }
+
+            }else if(Pipe > 1){
+                printf("%c",Course[i]);
+            }
+        }
+    }
+    fclose(AllStudents);
+    fclose(AllCourses);
     return StudentFound;
 }
 
-void EditStudent(int StudentId){
+void EditStudent(int StudentID){
     printf("EditStudent");
 }
 
-void DeleteStudent(int StudentId){
-    printf("EditStudent");
+int DeleteStudent(char StudentId){
+    char ThisStudetID[10];
+    int StudentFound = 1;
+    int Pipe;
+    ExistingAllStudents = fopen("data/All-Students.txt","r");
+    TempAllStudents = fopen("data/Temp-All-Students.txt","w");
+    LineLenth = 200;
+    char Student[LineLenth];
+
+    while(fgets(Student,LineLenth,ExistingAllStudents)){
+        for(i=0;Student[i] != '|'; i++){
+            ThisStudetID[i] = Student[i];
+        }
+        ThisStudetID[i] = '\0';
+        if(strcmp(StudentID,ThisStudetID) == 0){
+            StudentFound = 1;
+        }else{
+            StudentFound = 0;
+            fprintf(TempAllStudents,Student);
+        }
+    }
+    fclose(ExistingAllStudents);
+    fclose(TempAllStudents);
+
+    int CourseFound = 1;
+    ExistingAllCourses = fopen("data/All-Courses.txt","r");
+    TempAllCourses = fopen("data/Temp-All-Courses.txt","w");
+    char Course[LineLenth];
+
+    while(fgets(Course,LineLenth,ExistingAllCourses)){
+        for(i=0;Course[i] != '|'; i++){
+            ThisStudetID[i] = Course[i];
+        }
+        ThisStudetID[i] = '\0';
+        if(strcmp(StudentID,ThisStudetID) == 0){
+            CourseFound = 1;
+        }else{
+            fprintf(TempAllCourses,Course);
+            CourseFound = 0;
+        }
+    }
+    fclose(ExistingAllCourses);
+    fclose(TempAllCourses);
+    remove("data/All-Students.txt");
+    remove("data/All-Courses.txt");
+    rename("data/Temp-All-Students.txt", "data/All-Students.txt");
+    rename("data/Temp-All-Courses.txt", "data/All-Courses.txt");
+    if(StudentFound || CourseFound){
+        return 1;
+    }else{
+        return 0;
+    }
 }
+
+int DeleteAllStudents(){
+    remove("data/All-Students.txt");
+    remove("data/All-Courses.txt");
+    return 1;
+}
+
+
+
 
 bool IsAlreadyExists(char *GivenLine[30],char InfoType){
     return 1;
@@ -281,18 +405,3 @@ void UserGuideline(){
 void AboutUs(){
     printf("AboutUs");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
